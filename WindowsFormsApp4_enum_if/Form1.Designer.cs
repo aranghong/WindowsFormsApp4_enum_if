4ff7151347ea477d0e8e90ad9a2d2006bbb87f92
aranghong <har2129@yu.ac.kr>
2025-05-02 09:38:48 +0900

    Add project files.

---
+++ WindowsFormsApp4_enum_if/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace WindowsFormsApp4_enum_if
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(117, 48);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "가위";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(249, 48);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "바위";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(403, 47);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "보";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(117, 158);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(207, 173);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(434, 158);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(222, 173);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(117, 88);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(539, 52);
+            this.textBox3.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ WindowsFormsApp4_enum_if/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WindowsFormsApp4_enum_if
+{
+
+    public partial class Form1 : Form
+    {
+
+        int user = -1;
+        int computer = -1;
+        int userScore = 0;
+        int comScore = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //가위
+            textBox3.Text = "";
+            textBox3.Text += "사용자: " + "가위 \r\n";
+            user = 0;
+            game(user);
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //바위
+            textBox3.Text = "";
+            textBox3.Text += "사용자: " + "바위 \r\n";
+            user = 1;
+            game(user);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //보
+            textBox3.Text = "";
+            textBox3.Text += "사용자: " + "보 \r\n";
+            user = 2;
+            game(user);
+        }
+
+        int check()
+        {
+            if (userScore >= 3 || comScore >= 3)
+            {
+                textBox3.Text += "사용자: " + user + " \r\n";
+                textBox3.Text += "컴퓨터: " + computer + " \r\n";
+
+                textBox1.Text += "사용자 점수: " + userScore + " \r\n";
+                textBox2.Text += "컴퓨터 점수: " + comScore + " \r\n";
+
+                textBox1.Text = "";
+                textBox2.Text = "";
+                textBox3.Text = "3점 달성!";
+
+                userScore = 0;
+                comScore = 0;
+
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        void game(int choice)
+        {
+            Random r = new Random();
+            computer = (int)r.Next(0, 3);
+
+            int chk = check();
+
+            if (chk == 0)
+            {
+                if (computer == 0)
+                {
+                    textBox3.Text += "컴퓨터: " + "가위 \r\n";
+                    if (user == 0)
+                    {
+                        textBox1.Text += "사용자 점수: " + userScore + " \r\n";
+                        textBox2.Text += "컴퓨터 점수: " + comScore + " \r\n";
+
+                    }
+                    if (user == 1)
+                    {
+                        userScore++;
+                        textBox1.Text += "사용자 점수: " + userScore + " \r\n";
+                        textBox2.Text += "컴퓨터 점수: " + comScore + " \r\n";
+
+                    }
+                    if (user == 2)
+                    {
+                        comScore++;
+                        textBox1.Text += "사용자 점수: " + userScore + " \r\n";
+                        textBox2.Text += "컴퓨터 점수: " + comScore + " \r\n";
+
+                    }
+
+
+                }
+                else if (computer == 1)
+                {
+                    textBox3.Text += "컴퓨터: " + "바위 \r\n";
+                    if (user == 0)
+                    {
+                        comScore++;
+                        textBox1.Text += "사용자 점수: " + userScore + " \r\n";
+                        textBox2.Text += "컴퓨터 점수: " + comScore + " \r\n";
+
+                    }
+                    if (user == 1)
+                    {
+                        textBox1.Text += "사용자 점수: " + userScore + " \r\n";
+                        textBox2.Text += "컴퓨터 점수: " + comScore + " \r\n";
+
+                    }
+                    if (user == 2)
+                    {
+                        userScore++;
+                        textBox1.Text += "사용자 점수: " + userScore + " \r\n";
+                        textBox2.Text += "컴퓨터 점수: " + comScore + " \r\n";
+
+                    }
+                }
+                if (computer == 2)
+                {
+                    textBox3.Text += "컴퓨터: " + "보 \r\n";
+                    if (user == 0)
+                    {
+                        userScore++;
+                        textBox1.Text += "사용자 점수: " + userScore + " \r\n";
+                        textBox2.Text += "컴퓨터 점수: " + comScore + " \r\n";
+
+                    }
+                    if (user == 1)
+                    {
+                        comScore++;
+                        textBox1.Text += "사용자 점수: " + userScore + " \r\n";
+                        textBox2.Text += "컴퓨터 점수: " + comScore + " \r\n";
+
+                    }
+                    if (user == 2)
+                    {
+                        textBox1.Text += "사용자 점수: " + userScore + " \r\n";
+                        textBox2.Text += "컴퓨터 점수: " + comScore + " \r\n";
+
+                    }
+                }
+            }
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
