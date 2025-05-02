ce74db37f833abfabbe217ae8035a2989f61747f
aranghong <har2129@yu.ac.kr>
2025-05-02 09:41:11 +0900

    기존 코드에서 최적화1

--- WindowsFormsApp4_enum_if/Form1.cs
+++ WindowsFormsApp4_enum_if/Form1.cs
@@ -1,19 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace WindowsFormsApp4_enum_if
 {
-
     public partial class Form1 : Form
     {
+        enum RPS { Scissors = 0, Rock = 1, Paper = 2 }
 
         int user = -1;
         int computer = -1;
@@ -23,158 +15,102 @@ namespace WindowsFormsApp4_enum_if
         public Form1()
         {
             InitializeComponent();
-
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            //가위
+            user = (int)RPS.Scissors;
             textBox3.Text = "";
-            textBox3.Text += "사용자: " + "가위 \r\n";
-            user = 0;
+            textBox3.Text += "사용자: 가위\r\n";
             game(user);
-
-
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            //바위
+            user = (int)RPS.Rock;
             textBox3.Text = "";
-            textBox3.Text += "사용자: " + "바위 \r\n";
-            user = 1;
+            textBox3.Text += "사용자: 바위\r\n";
             game(user);
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            //보
+            user = (int)RPS.Paper;
             textBox3.Text = "";
-            textBox3.Text += "사용자: " + "보 \r\n";
-            user = 2;
+            textBox3.Text += "사용자: 보\r\n";
             game(user);
+        }
+
+        void game(int choice)
+        {
+            Random r = new Random();
+            computer = r.Next(0, 3);
+
+            if (check() == 1) return;
+
+            ShowComputerChoice();
+
+            int result = Judge((RPS)user, (RPS)computer);
+            if (result == 1)
+            {
+                userScore++;
+            }
+            else if (result == -1)
+            {
+                comScore++;
+            }
+
+            PrintScores();
+        }
+
+        int Judge(RPS user, RPS computer)
+        {
+            if (user == computer) return 0;
+
+            if ((user == RPS.Scissors && computer == RPS.Paper) ||
+                (user == RPS.Rock && computer == RPS.Scissors) ||
+                (user == RPS.Paper && computer == RPS.Rock))
+            {
+                return 1; // 사용자 승
+            }
+
+            return -1; // 컴퓨터 승
+        }
+
+        void ShowComputerChoice()
+        {
+            string comStr = computer switch
+            {
+                (int)RPS.Scissors => "가위",
+                (int)RPS.Rock => "바위",
+                (int)RPS.Paper => "보",
+                _ => "?"
+            };
+            textBox3.Text += $"컴퓨터: {comStr}\r\n";
+        }
+
+        void PrintScores()
+        {
+            textBox1.Text = $"사용자 점수: {userScore}\r\n";
+            textBox2.Text = $"컴퓨터 점수: {comScore}\r\n";
         }
 
         int check()
         {
             if (userScore >= 3 || comScore >= 3)
             {
-                textBox3.Text += "사용자: " + user + " \r\n";
-                textBox3.Text += "컴퓨터: " + computer + " \r\n";
-
-                textBox1.Text += "사용자 점수: " + userScore + " \r\n";
-                textBox2.Text += "컴퓨터 점수: " + comScore + " \r\n";
-
                 textBox1.Text = "";
                 textBox2.Text = "";
-                textBox3.Text = "3점 달성!";
+                textBox3.Text = "3점 달성!\r\n점수 초기화됩니다.";
 
                 userScore = 0;
                 comScore = 0;
-
                 return 1;
             }
-            else
-            {
-                return 0;
-            }
+            return 0;
         }
 
-        void game(int choice)
-        {
-            Random r = new Random();
-            computer = (int)r.Next(0, 3);
-
-            int chk = check();
-
-            if (chk == 0)
-            {
-                if (computer == 0)
-                {
-                    textBox3.Text += "컴퓨터: " + "가위 \r\n";
-                    if (user == 0)
-                    {
-                        textBox1.Text += "사용자 점수: " + userScore + " \r\n";
-                        textBox2.Text += "컴퓨터 점수: " + comScore + " \r\n";
-
-                    }
-                    if (user == 1)
-                    {
-                        userScore++;
-                        textBox1.Text += "사용자 점수: " + userScore + " \r\n";
-                        textBox2.Text += "컴퓨터 점수: " + comScore + " \r\n";
-
-                    }
-                    if (user == 2)
-                    {
-                        comScore++;
-                        textBox1.Text += "사용자 점수: " + userScore + " \r\n";
-                        textBox2.Text += "컴퓨터 점수: " + comScore + " \r\n";
-
-                    }
-
-
-                }
-                else if (computer == 1)
-                {
-                    textBox3.Text += "컴퓨터: " + "바위 \r\n";
-                    if (user == 0)
-                    {
-                        comScore++;
-                        textBox1.Text += "사용자 점수: " + userScore + " \r\n";
-                        textBox2.Text += "컴퓨터 점수: " + comScore + " \r\n";
-
-                    }
-                    if (user == 1)
-                    {
-                        textBox1.Text += "사용자 점수: " + userScore + " \r\n";
-                        textBox2.Text += "컴퓨터 점수: " + comScore + " \r\n";
-
-                    }
-                    if (user == 2)
-                    {
-                        userScore++;
-                        textBox1.Text += "사용자 점수: " + userScore + " \r\n";
-                        textBox2.Text += "컴퓨터 점수: " + comScore + " \r\n";
-
-                    }
-                }
-                if (computer == 2)
-                {
-                    textBox3.Text += "컴퓨터: " + "보 \r\n";
-                    if (user == 0)
-                    {
-                        userScore++;
-                        textBox1.Text += "사용자 점수: " + userScore + " \r\n";
-                        textBox2.Text += "컴퓨터 점수: " + comScore + " \r\n";
-
-                    }
-                    if (user == 1)
-                    {
-                        comScore++;
-                        textBox1.Text += "사용자 점수: " + userScore + " \r\n";
-                        textBox2.Text += "컴퓨터 점수: " + comScore + " \r\n";
-
-                    }
-                    if (user == 2)
-                    {
-                        textBox1.Text += "사용자 점수: " + userScore + " \r\n";
-                        textBox2.Text += "컴퓨터 점수: " + comScore + " \r\n";
-
-                    }
-                }
-            }
-
-        }
-
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
-            
-        }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-            
-        }
+        private void textBox2_TextChanged(object sender, EventArgs e) { }
+        private void textBox1_TextChanged(object sender, EventArgs e) { }
     }
 }
